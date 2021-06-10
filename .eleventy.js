480b37b657564fcfbb40c172b491b4a3bb4d887f
Juan José Vázquez <juanjo.vazquez.delgado@tecsisa.com>
2021-06-10 20:29:15 +0200

    reflexiones sobre Scala

--- .eleventy.js
+++ .eleventy.js
@@ -5,9 +5,11 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const timeToRead = require('eleventy-plugin-time-to-read');
 
 module.exports = function(eleventyConfig) {
   // Add plugins
+  eleventyConfig.addPlugin(timeToRead);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
